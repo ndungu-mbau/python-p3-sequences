00ef03859c77a6a14df34f644e4ad23c243279ed
Nelson Mbau <mbau.ndungu@gmail.com>
2024-05-22 11:55:55 +0300

    feat: write fibonacci sequence code

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+import ipdb 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        fib = [0] * length
+        fib[1] = 1
+        for i in range(2, length):
+            fib[i] = fib[i-1] + fib[i-2]
+        print(fib)
